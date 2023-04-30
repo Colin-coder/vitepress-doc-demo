80b53669078d43c73314b1389e10a1e3a379138a
nk <1527190079@qq.com>
2023-04-30 22:51:09 +0800

    add blog demo

---
+++ .vitepress/config.ts
@@ -0,0 +1,63 @@
+import { defineConfig } from "vitepress"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "ChatKit Docs",
+  description: "Documentation for ChatKit",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "ChatKit", link: "https://chatkit.app" },
+    ],
+
+    sidebar: [
+      {
+        text: "Guide",
+        items: [
+          {
+            text: "Chat over External Documents",
+            link: "/chat-external-documents",
+          },
+          {
+            text: "Prompt Templates",
+            link: "/prompt-templates",
+          },
+          {
+            text: "Shortcuts",
+            link: "/shortcuts",
+          },
+          {
+            text: "Credits",
+            link: "/credits",
+          },
+        ],
+      },
+      {
+        text: "Tools",
+        items: [
+          {
+            text: "Overview",
+            link: `/tools/overview`,
+          },
+          {
+            text: "Google Search",
+            link: "/tools/google-search",
+          },
+          {
+            text: "Request Get",
+            link: "/tools/request-get",
+          },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/egoist/chatkit-docs" },
+    ],
+
+    editLink: {
+      pattern: "https://github.com/egoist/chatkit-docs/blob/main/:path",
+    },
+  },
+})
---
+++ credits.md
@@ -0,0 +1,15 @@
+# Credits
+
+ChatKit [Premium](https://chatkit.app/#pricing) has the option to buy credits and use them instead of providing an OpenAI API Key.
+
+![preview](https://fastly.jsdelivr.net/gh/egoist-bot/images@main/uPic/Qno7mO.jpeg)
+
+## Pricing
+
+$5 credits cost exactly $5 USD, different API will cost a different amount of credits, Our pricing is based on OpenAI pricing plus 30% fee, the final price is as follows:
+
+| API            | Cost                                                               |
+| -------------- | ------------------------------------------------------------------ |
+| GPT-3.5        | $0.0026 per 1000 tokens                                            |
+| GPT-4          | prompt: $0.039 per 1000 tokens, completion: $0.078 per 1000 tokens |
+| Speech-to-Text | $0.0078 per minute                                                 |
